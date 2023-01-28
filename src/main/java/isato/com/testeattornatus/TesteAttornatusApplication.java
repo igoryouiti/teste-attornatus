78254d313a54b11be9cbd438aebfc27a367fb192
igoryouiti <igoryouiti@hotmail.com>
2023-01-27 21:09:32 -0300

    primeiro commit

---
+++ src/main/java/isato/com/testeattornatus/TesteAttornatusApplication.java
@@ -0,0 +1,13 @@
+package isato.com.testeattornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAttornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAttornatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isato/com/testeattornatus/TesteAttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package isato.com.testeattornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

33e63cd1c1f03201abf9dc62de03965706dd41f3
igoryouiti <igoryouiti@hotmail.com>
2023-01-28 16:43:06 -0300

    removed not used dependencie

--- src/main/java/isato/com/testeattornatus/model/Pessoa.java
+++ src/main/java/isato/com/testeattornatus/model/Pessoa.java
@@ -4,7 +4,6 @@ package isato.com.testeattornatus.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Date;

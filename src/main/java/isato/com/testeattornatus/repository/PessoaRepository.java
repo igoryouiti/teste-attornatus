6b913700d7cededda49cc0f04742406982dae102
igoryouiti <igoryouiti@hotmail.com>
2023-01-29 11:29:36 -0300

    created Pessoa and Endereco repository

---
+++ src/main/java/isato/com/testeattornatus/repository/PessoaRepository.java
@@ -0,0 +1,11 @@
+package isato.com.testeattornatus.repository;
+
+import isato.com.testeattornatus.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+
+}

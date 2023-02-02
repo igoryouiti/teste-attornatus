40be892bd666eae1e8177cd49e1b735de15cba28
igoryouiti <igoryouiti@hotmail.com>
2023-02-01 21:48:29 -0300

    add querys

--- src/main/java/isato/com/testeattornatus/repository/EnderecoRepository.java
+++ src/main/java/isato/com/testeattornatus/repository/EnderecoRepository.java
@@ -2,10 +2,20 @@ package isato.com.testeattornatus.repository;
 
 import isato.com.testeattornatus.model.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 
+    @Query(value = "SELECT * FROM tb_enderecos WHERE fk_pessoas_id = ?1", nativeQuery = true)
+    public Optional<List<Endereco>> findAllEnderecoByPessoaId(Long id);
+
+    @Query(value = "SELECT * FROM tb_enderecos WHERE fk_pessoas_id = ?1 and principal = true", nativeQuery = true)
+    public Optional<Endereco> findEnderecoPrincipalByPessoa(Long id);
 
 }

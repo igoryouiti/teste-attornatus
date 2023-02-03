af7582fc4b2a9957ee2feb76a3c95331316b8610
igoryouiti <igoryouiti@hotmail.com>
2023-02-03 18:31:55 -0300

    Endereco Repository Test Development

---
+++ src/test/java/isato/com/testeattornatus/repository/EnderecoRepositoryTest.java
@@ -0,0 +1,61 @@
+package isato.com.testeattornatus.repository;
+
+import isato.com.testeattornatus.model.Endereco;
+import isato.com.testeattornatus.model.Pessoa;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class EnderecoRepositoryTest {
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @BeforeAll
+    void start(){
+        enderecoRepository.deleteAll();
+
+        Pessoa pessoa = pessoaRepository.save(new Pessoa("Mariana", new Date("1994/05/12")));
+
+        enderecoRepository.save(new Endereco("Rua Joao", "69885410", "55", "Mogi das Cruzes", false));
+        enderecoRepository.save(new Endereco("Rua Kleber", "54789888", "7", "Mogi das Cruzes", false, pessoa));
+        enderecoRepository.save(new Endereco("Rua Gloria", "58749856", "12", "Mogi das Cruzes", true, pessoa));
+        enderecoRepository.save(new Endereco("Rua Maria", "65201478", "57", "Mogi das Cruzes", false, pessoa));
+    }
+
+    @Test
+    @DisplayName("Return 3 Endereco")
+    public void mustReturnAllEnderecoFromPessoa(){
+
+        List<Endereco> enderecos = enderecoRepository.findAllEnderecoByPessoaId(1L).get();
+
+        assertEquals(3, enderecos.size());
+
+    }
+
+    @Test
+    @DisplayName("Return 1 Endereco principal == true from Pessoa")
+    public void mustReturnEnderecoPrincipalFromPessoa(){
+
+        Endereco endereco = enderecoRepository.findEnderecoPrincipalByPessoa(1L).get();
+
+        assertTrue(endereco.getPrincipal().equals(true));
+
+    }
+
+}
